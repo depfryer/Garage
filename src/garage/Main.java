32e9c1d576bae836dc527ca3734cd8616fe958e8
maxime <maxime.leblond76@gmail.com>
2017-01-24 14:35:21 +0100

    création menu

--- src/garage/Main.java
+++ src/garage/Main.java
@@ -4,6 +4,8 @@
  */
 package garage;
 
+import java.util.Scanner;
+
 /**
  *
  * @author duche
@@ -14,6 +16,33 @@ public class Main {
       * @param args the command line arguments
       */
      public static void main(String[] args) {
-          // TODO code application logic here
+          int choix;
+          
+          do {               
+               System.out.println("1: yolo");
+               System.out.println("2: swag");
+               System.out.println("0:Quitter");
+
+               choix = new Scanner(System.in).nextInt();
+               System.out.println(choix);
+               
+               switch (choix){
+                   case 1:
+                        System.out.println("vehicule");
+                        System.out.println("nom");
+                       
+                        String nom = new Scanner(System.in).nextLine();
+                        
+                        System.out.println("panne");
+                        String panne = new Scanner(System.in).nextLine();
+                        System.out.println("vehicule  "+nom+ "  panne "+panne);
+                        break;
+                    case 2:
+                         System.out.println("s");
+                         break;
+                    default :
+                         break;
+               }
+          } while (choix != 0);
      }
 }
